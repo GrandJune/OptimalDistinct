6daec3fbc625533c64b526f3c74083871a912af3
毕夏 <978260577@qq.com>
2024-07-20 22:56:53 +0800

    first version

--- Cluster/Detect_language.py
+++ Cluster/Detect_language.py
@@ -9,34 +9,39 @@ import pandas as pd
 
 # Load and preprocess data
 text_file = r"Detail_99_CleanData_0625.csv"
-df = pd.read_csv(text_file)
+text_file_list = ["Detail_99_CleanData_0510.csv", "Detail_99_CleanData_0507.csv",
+                  "Detail_99_CleanData_0514.csv", "Detail_99_CleanData_0521.csv", "Detail_99_CleanData_0528.csv",
+                  "Detail_99_CleanData_0604.csv", "Detail_99_CleanData_0611.csv", "Detail_99_CleanData_0618.csv",
+                  "Detail_99_CleanData_0625.csv"]
+for text_file in text_file_list:
+    df = pd.read_csv(text_file)
 
-# Function to detect primary language of a sentence
-def detect_primary_language(sentence):
-    try:
-        # Initialize DetectorFactory to increase reliability
-        DetectorFactory.seed = 0
-        # Skip empty or very short texts
-        if len(sentence.strip()) < 3:  # Adjust the threshold as needed
+    # Function to detect primary language of a sentence
+    def detect_primary_language(sentence):
+        try:
+            # Initialize DetectorFactory to increase reliability
+            DetectorFactory.seed = 0
+            # Skip empty or very short texts
+            if len(sentence.strip()) < 3:  # Adjust the threshold as needed
+                return "None"
+            # Detect language of the sentence
+            lang = detect(sentence)
+            return lang
+        except Exception as e:
+            print(f"Error detecting language: {sentence}")
             return "None"
-        # Detect language of the sentence
-        lang = detect(sentence)
-        return lang
-    except Exception as e:
-        print(f"Error detecting language: {sentence}")
-        return "None"
 
-# Create a new column to store primary language
-df['Primary_Language'] = None
+    # Create a new column to store primary language
+    df['Primary_Language'] = None
 
-# Iterate over each row and apply the language detection function
-for index, row in df.iterrows():
-    if pd.notna(row['Description']):
-        df.at[index, 'Primary_Language'] = detect_primary_language(row['Description'])
+    # Iterate over each row and apply the language detection function
+    for index, row in df.iterrows():
+        if pd.notna(row['Description']):
+            df.at[index, 'Primary_Language'] = detect_primary_language(row['Description'])
 
-# Save the DataFrame with primary language identified as a new column in the original CSV file
-output_file = "Detail_99_CleanData_0625_with_language.csv"
-df.to_csv(output_file, index=False)
+    # Save the DataFrame with primary language identified as a new column in the original CSV file
+    output_file = "Detail_99_CleanData_0625_with_language.csv"
+    df.to_csv(output_file, index=False)
 
 # Display the DataFrame with primary language identified for each sentence
 # print(df[['Description', 'Primary_Language']])
