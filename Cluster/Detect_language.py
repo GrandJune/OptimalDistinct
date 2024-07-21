a13543d6b3921584a37313d4743ade57e39a6883
毕夏 <978260577@qq.com>
2024-07-21 18:13:31 +0800

    first version

--- Cluster/Detect_language.py
+++ Cluster/Detect_language.py
@@ -8,11 +8,11 @@ from langdetect import detect, DetectorFactory
 import pandas as pd
 
 # Load and preprocess data
-text_file = r"Detail_99_CleanData_0625.csv"
-text_file_list = ["Detail_99_CleanData_0510.csv", "Detail_99_CleanData_0507.csv",
-                  "Detail_99_CleanData_0514.csv", "Detail_99_CleanData_0521.csv", "Detail_99_CleanData_0528.csv",
-                  "Detail_99_CleanData_0604.csv", "Detail_99_CleanData_0611.csv", "Detail_99_CleanData_0618.csv",
-                  "Detail_99_CleanData_0625.csv"]
+text_file = r"./data/Detail_99_CleanData_0625.csv"
+text_file_list = ["Detail_99_CleanData_0507.csv", "Detail_99_CleanData_0514.csv", "Detail_99_CleanData_0521.csv",
+                  "Detail_99_CleanData_0528.csv", "Detail_99_CleanData_0604.csv", "Detail_99_CleanData_0611.csv",
+                  "Detail_99_CleanData_0618.csv", "Detail_99_CleanData_0625.csv"]
+text_file_list = [r"./data/" + text for text in text_file_list]
 for text_file in text_file_list:
     df = pd.read_csv(text_file)
 
